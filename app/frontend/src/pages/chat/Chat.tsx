37d515e1091096677d49c27e42beb750e600f24d
Nayan Banik <129023126+nynbnk@users.noreply.github.com>
2023-12-03 23:32:30 -0600

    Update Chat.tsx

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -235,9 +235,10 @@ const Chat = () => {
                 <div className={styles.chatContainer}>
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
-                            <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
-                            <h1 className={styles.chatEmptyStateTitle}>Chat with your data</h1>
-                            <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
+                            <img src="https://industrialtalk.com/wp-content/uploads/2021/02/UWM-CSI-4clr-1235-Logo.png" alt="Chat logo" width="100" />
+                            {/* <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" /> */}
+                            <h1 className={styles.chatEmptyStateTitle}>Virtual Guide to the CSI Advanced Manufacturing Test Bed</h1>
+                            <h2 className={styles.chatEmptyStateSubtitle}>Ask anything about CSI or try an example</h2>
                             <ExampleList onExampleClicked={onExampleClicked} />
                         </div>
                     ) : (
